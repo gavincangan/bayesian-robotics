f7e415a9c40f537e76af7554c2c8ffef8f1813ea
Abdullah Abduldayem <abdullahdayem@gmail.com>
2018-10-15 21:59:05 -0400

    Restructured repo, created workspace, perform ball detection.

--- README.md
+++ README.md
@@ -1,26 +1,135 @@
 # ME5524 Bayesian Robotics - Fall 2018
 
-## Team 3
-- Abdullah Abdul-Dayem
-- Barnabas Gavin Cangan
-- Lisheng Yang
+Workspace for the MBZIRC 2020 Challenge 3. The objective of this package is to locate a fire using the Clearpath Jackal UGV and navigate towards it.
 
-## Timeline
+## Table of Contents
+1. [Installation](#1-installation)
+2. [Operation](#2-operation)
+3. [Timeline](#3-timeline)
+4. [TODO](#4-todo)
+5. [Contributiors](#5-contributors)
 
-2018/09/26
+## 1. Installation
+### Install prerequisites
+```bash
+sudo apt-get update
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt-get install protobuf-compiler python-pip python-toml python-jinja2 python-catkin-tools
+sudo pip install --upgrade pip
+sudo pip install -U catkin_tool
+
+sudo apt-get install gstreamer1.0-tools libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev
+```
+
+### Install ROS packages
+```bash
+sudo apt-get install ros-indigo-hokuyo-node
+sudo apt-get install ros-kinetic-cv-bridge
+```
+
+### Configure base station to connect with Jackal
+Edit `/etc/hosts` and add the following line:
+
+```
+192.168.1.11    cpr-jackal
+```
+
+### Install this workspace
+```bash
+cd ~
+git clone https://github.com/gavincangan/bayesian-robotics.git
+cd ~/bayesian-robotics
+```
+
+### Configure Environment
+
+Add this to your `~/.bashrc` file.
+
+```bash
+nano ~/.bashrc
+
+## Add the following, save and quit
+source $HOME/bayesian-robotics/br_f18_project_ws/devel/setup.bash
+```
+
+### Download Workspace Dependencies
+```bash
+cd ~/bayesian-robotics/br_f18_project_ws
+wstool update -t src
+```
+
+### Build Workspace
+```
+cd ~/bayesian-robotics/br_f18_project_ws
+catkin build
+```
+
+
+## 2. Operation
+
+### Configure ROS MASTER
+The following must be run on each terminal to communicate properly with the jackal. To avoid doing this every time, you can include these in you `~/.bashrc` file.
+```
+export ROS_MASTER_URI=http://cpr-jackal:11311
+export ROS_IP=<your computer's IP address>
+```
+
+### Start the webcam and lidar
+```
+roslaunch mbz_c3_jackal gscam.launch
+rosrun hokuyo_node hokuyo_node
+```
+
+
+### Getting the Jackal to work with keyboard teleop
+- Used this package: https://wiki.ros.org/teleop_twist_keyboard
+```
+### On Ubuntu, ROS Kinetic
+sudo apt-get install ros-kinetic-teleop-twist-keyboard
+```
+- Once we had that set up, we could fire up the node and control Jackal using the keyboard
+
+```
+source <path-to-catkin/devel/setup.bash>
+rosrun teleop_twist_keyboard teleop_twist_keyboard.py
+```
+
+
+## 3. Timeline
+
+### 2018/09/26
 - Team formed
 
-2018/09/29
+### 2018/09/29
 - Project kickstart meeting
 - Got it moving with keyboard teleop, attempted LIDAR and webcam, installed hector
 
-2018/10/03
+### 2018/10/03
 - Meeting planned
 - Get data from webcam & LIDAR - Done
 - LIDAR & camera mounts designed. Printing in progress
 - Gavin learned how to use FreeCAD :)
 
-## TODO
+### 2018/10/03
+- Meeting planned
+- Get data from webcam & LIDAR - Done
+- LIDAR & camera mounts designed. Printing in progress
+- Gavin learned how to use FreeCAD :)
+
+### 2018/10/10
+
+
+### 2018/10/15
+- Implimented ball detection
+- Restructured repository
+
+## 4. TODO
 * Mount LIDAR and webcam (3d design + print)
 * Static IP for wifi
 * Run hector
+
+## 5. Contributors
+Team 3
+- [Abdullah Abdul-Dayem](https://github.com/Abdullah-Abduldayem)
+- [Barnabas Gavin Cangan](https://github.com/gavincangan)
+- Lisheng Yang
---
+++ br_f18_project_ws/src/mbz_c3_jackal/script/ball_detection.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import sys
+import cv2
+import numpy as np
+from math import pi
+from matplotlib import pyplot as plt
+import os
+
+import roslib
+roslib.load_manifest('mbz_c3_jackal')
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+
+class image_converter:
+    def __init__(self):
+        self.image_pub = rospy.Publisher("image_topic_2",Image)
+        
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/camera/image_raw",Image,self.callback)
+
+        ## Manually set exposure for camera
+        os.system("v4l2-ctl -d /dev/video1 --set-ctrl=exposure_auto=1")
+        os.system("v4l2-ctl -d /dev/video1 --set-ctrl=exposure_absolute=170")
+
+    def callback(self,data):
+        try:
+            img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+        
+        img_threshold = self.colorThreshold(img)
+        circles = self.detectCircles(img_threshold)
+        
+        ## Draw circles on final image
+        img_final = img.copy()
+        for c in circles:
+            center = c[0]
+            radius = c[1]
+            
+            cv2.circle(img_final, center, int(radius), (0, 255, 255), 2)
+            cv2.circle(img_final, center, 5, (255, 0, 255), -1)
+            
+            
+            ## Display some extra information about the object on-screen
+            
+            ## Get the estimated distance
+            ## This is the number of pixels we see at 1m for a ball of radius 10cm
+            pixels_at_1m = 142.0 ## Radius = 10cm
+            pixels_at_1m = 163.3 ## Radius = 11.5cm
+            
+            z_est = pixels_at_1m/radius
+            
+            #self.drawText(img_final, "Radius: {:3.1f} px".format(radius), center[0], center[1])
+            self.drawText(img_final, "Distance: {:1.2f}m".format(z_est), center[0]+30, center[1]-int(radius))
+            
+        
+        cv2.imshow("Image window", img_final)
+        cv2.waitKey(1)
+        
+        try:
+            self.image_pub.publish(self.bridge.cv2_to_imgmsg(img, "bgr8"))
+        except CvBridgeError as e:
+            print(e)
+
+
+    def drawText(self, img, text, x, y):
+        cv2.putText(img, text, (x, y), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.7, color=(  0,  0,  0), lineType=cv2.LINE_AA, thickness=5, bottomLeftOrigin=False)
+        cv2.putText(img, text, (x, y), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.7, color=(255,255,255), lineType=cv2.LINE_AA, thickness=2, bottomLeftOrigin=False)
+    
+    
+    def colorThreshold(self, img):  
+        ## Get HSV image
+        ## NOTE: We purposefully did RGB and not BGR. This maps red to the B channel
+        hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        
+        ## Max out the saturation (not very important) and brightness (not important)
+        h, s, v = cv2.split(hsv)
+        #s.fill(255)
+        v.fill(255)
+
+        ## Merge HSV channels back to a single image
+        hsv = cv2.merge([h, s, v])
+        
+        ## Go back to RGB space
+        img = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+        
+        ## Let's try to find red objects
+        center = 115 #110-120
+        bandwidth = 5
+        
+        _, mask1 = cv2.threshold(h,thresh=center-bandwidth, maxval=1, type=cv2.THRESH_BINARY)
+        _, mask2 = cv2.threshold(h,thresh=center+bandwidth, maxval=1, type=cv2.THRESH_BINARY_INV)
+        mask_hue = cv2.bitwise_and(mask1, mask2)
+        
+        ## And make sure that they're saturated
+        _, mask_sat = cv2.threshold(s,thresh=80, maxval=1, type=cv2.THRESH_BINARY)
+        
+        ## Make the final mask
+        mask = cv2.bitwise_and(mask_hue, mask_sat)
+        
+        ## Apply the mask
+        img_masked = cv2.bitwise_and(img, img, mask = mask)
+        
+        return img_masked
+    
+    
+    def detectCircles(self, img):
+        ## Get a greyscale version of the image
+        img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        
+        _, mask = cv2.threshold(img_gray,thresh=20, maxval=1, type=cv2.THRESH_BINARY)
+        
+        min_radius = 10
+        min_area = 400
+        
+        # find contours in the mask and initialize the current (x, y) center of the ball
+        #cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]
+
+        circles_out = []
+        ## Process all contours
+        for c in cnts:
+            if cv2.contourArea(c) < min_area:
+                continue
+            
+            ## Find bounding circle
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            center = (int(x), int(y))
+            
+            ## Draw contour
+            #cv2.drawContours(img, [c], 0, (0, 255, 0), 2)
+            
+            ## Only proceed if the radius meets a minimum size
+            if radius < min_radius:
+                continue
+            
+            ## Make sure this is actually a circle (ie. fills up some minimum percentage of bounding circle)
+            area_bounding = pi*(radius**2)
+            area_ratio = cv2.contourArea(c)/area_bounding
+            
+            if (area_ratio < 0.7):
+                continue
+            
+            ## Save this value
+            circles_out.append([center, radius])
+        
+        return circles_out
+
+def main(args):
+    ic = image_converter()
+    rospy.init_node('ball_detection', anonymous=True)
+    
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
